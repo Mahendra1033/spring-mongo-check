1186b1983b5714edab708bfcea3ae9a1049cedb4
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-10-13 23:07:14 +0100

    spring project with mongo

---
+++ src/main/java/com/golearntolead/mongo/SpringMongoExampleTwoApplication.java
@@ -0,0 +1,13 @@
+package com.golearntolead.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoExampleTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoExampleTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golearntolead/mongo/SpringMongoExampleTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golearntolead.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoExampleTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
